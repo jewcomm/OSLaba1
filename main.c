017b883e461123da4c014647e3ae06a190511dd5
Konovalov Vladimir <wladimir.konowalow411@mail.ru>
2022-02-27 20:54:42 +0300

    declared function archive and wrote its call

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+void archive(char *dir, int outputDescriptor); //функция архивирует директорию dir, записывая результат в открытый файл с дескриптором outputDescriptor
 
 int main(int argc, char* argv[])
 {
-	
+	int outputDescriptor; 
+	if((outputDescriptor = open(argv[2], O_WRONLY|O_CREAT|O_TRUNC, S_IWUSR|S_IRUSR|S_IRGRP|S_IROTH)) < 0)
+	{
+		printf("error opening file %s\n", argv[2]);
+		return -1;
+	}
+	archive(argv[1], outputDescriptor);
+	if(close(outputDescriptor) == -1) printf("error closing file %s\n", argv[2]);
 	return 0;
+}
+
+void archive(char *dir, int outputDescriptor)
+{
+	
 }
