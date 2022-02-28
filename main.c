f6591f0acfc0e2c30e9ba6ff8cfcb1ede8ad9033
Konovalov Vladimir <wladimir.konowalow411@mail.ru>
2022-02-28 21:24:22 +0300

    defined function archive

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
 
 void archive(char *dir, int outputDescriptor); //функция архивирует директорию dir, записывая результат в открытый файл с дескриптором outputDescriptor
 
@@ -15,11 +18,62 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 	archive(argv[1], outputDescriptor);
-	if(close(outputDescriptor) == -1) printf("error closing file %s\n", argv[2]);
+	if(close(outputDescriptor) < 0) printf("error closing file %s\n", argv[2]);
 	return 0;
 }
 
 void archive(char *dir, int outputDescriptor)
 {
+	if(write(outputDescriptor, dir, strlen(dir)) != strlen(dir)) printf("Write error\n");
+	if(write(outputDescriptor, "<", 1) != 1) printf("Write error\n");
 	
+	//Открытие каталога
+	DIR *dirp;
+	if ((dirp = opendir(dir)) == NULL)
+	{
+		printf("cannot open directory: %s\n", dir);
+		return;
+	}
+	chdir(dir);
+	
+	struct dirent *entry; //указатель для доступа к элементу каталога
+	struct stat statbuf;
+	//Цикл по элементам каталога
+	while((entry = readdir(dirp)) != NULL)
+	{
+		lstat(entry->d_name, &statbuf);
+		if (S_ISDIR(statbuf.st_mode)) //если текущий элемент - каталог, то рекурсивный вызов
+		{
+			if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0) continue; //Находит каталог, но игнорирует . и ..
+			archive(entry->d_name, outputDescriptor); //Рекурсивный вызов
+		}
+		else //иначе запись данных о текущем элементе в выходной файл
+		{
+			int fileDscr;
+			if((fileDscr = open(entry->d_name, O_RDONLY)) < 0)
+			{
+				printf("error opening file %s\n", entry->d_name);
+				continue;
+			}
+			if(write(outputDescriptor, entry->d_name, strlen(entry->d_name)) != strlen(entry->d_name)) printf("Write error\n");
+			if(write(outputDescriptor, "|", 1) != 1) printf("Write error\n");
+			if(write(outputDescriptor, &(statbuf.st_size), sizeof(off_t)) != sizeof(off_t)) printf("Write error\n");
+			char *buf;
+			if((buf = malloc(statbuf.st_size)) == NULL)
+			{
+				printf("memory allocation error\n");
+			}
+			if(read(fileDscr, buf, statbuf.st_size)!=statbuf.st_size)
+			{
+				printf("Read error\n");
+			}
+			if(close(fileDscr) < 0) printf("error closing file %s\n", entry->d_name);
+			if(write(outputDescriptor, buf, statbuf.st_size) != statbuf.st_size) printf("Write error\n");
+			free(buf);
+		}
+	}
+	if(write(outputDescriptor, ">", 1) != 1) printf("Write error\n");
+	chdir("..");
+	closedir(dirp);
 }
+
