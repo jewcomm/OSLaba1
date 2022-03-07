efb78a826493dacb2d9fc9d4cab30ac7cc8b2b14
Konovalov Vladimir <wladimir.konowalow411@mail.ru>
2022-03-07 21:31:28 +0300

    Now the archive file is read in batches

--- main.c
+++ main.c
@@ -7,9 +7,13 @@
 #include <dirent.h>
 #include <string.h>
 
-void archive(char *dir, int outputDescriptor); //функция архивирует директорию dir, записывая результат в открытый файл с дескриптором outputDescriptor
-char* unzip(char *bufArch); //bufArch - строка, в которую был скопирован файл архива
+#define PART_SIZE 4096
+#define NAME_MAX_LEN 255
 
+void archive(char *dir, int outputDescriptor); //функция архивирует директорию dir, записывая результат в открытый файл с дескриптором outputDescriptor
+char* unzip(int inputDescriptor, char *buf, char* currp, char *endp); //inputDescriptor - дескриптор файла архива, buf - буфер, currp - текущая позиция в этом буфере
+
+char* readmore(int inputDescriptor, char *buf, char *currp, char *endp); //прочитает из файла в буфер следующие PART_SIZE байт, при этом оставляя в буфере ещё не обработаные данные
 char* searchEntry(char *str); //ищет в строке первое вхождение символов '|' или '<', если не находит, возвращает NULL
 
 int main(int argc, char* argv[])
@@ -52,27 +56,26 @@ int main(int argc, char* argv[])
 	}
 	else //иначе разархивация
 	{
-		char *bufArch;
-		if((bufArch = malloc(statbuf.st_size+1)) == NULL)
+		char *buf;
+		/*буфер, в котором отведено NAME_MAX_LEN байт для запоминания имени файла и PART_SIZE байт для чтения архива по частям, а также 1 байт для '\0'*/
+		if((buf = malloc(NAME_MAX_LEN + PART_SIZE + 1)) == NULL)
 		{
 			printf("memory allocation error\n");
 			return -2;
 		}
+		buf[0] = '\0';
+		
+		/*открытие файла архива*/
 		int inputDescriptor; 
 		if((inputDescriptor = open(argv[input], O_RDONLY)) < 0)
 		{
 			printf("error opening file %s\n", argv[input]);
 			return -3;
 		}
-		if(read(inputDescriptor, bufArch, statbuf.st_size) != statbuf.st_size)
-		{
-			printf("Read error\n");
-			return -4;
-		}
-		bufArch[statbuf.st_size] = '\0';
+
 		chdir(argv[output]);
-		unzip(bufArch);
-		free(bufArch);
+		unzip(inputDescriptor, buf, buf, buf);
+		free(buf);
 	}
 	
 	return 0;
@@ -133,38 +136,76 @@ void archive(char *dir, int outputDescriptor)
 	closedir(dirp);
 }
 
-char* unzip(char *bufArch)
+char* unzip(int inputDescriptor, char *buf, char* currp, char *endp)
 {
 	char *strp;
-	while((strp = searchEntry(bufArch)) != NULL)
+	do
 	{
-		if(strp[0] == '<')
+		if((strp = searchEntry(currp)) == NULL)
 		{
-			strp[0] = '\0';
-			mkdir(bufArch, S_IWUSR|S_IRUSR|S_IXUSR|S_IRGRP|S_IWGRP|S_IXGRP|S_IROTH|S_IWOTH|S_IXOTH);
-			chdir(bufArch);
-			bufArch = unzip(strp + 1);
+			endp = readmore(inputDescriptor, buf, currp, endp);
+			currp = buf;
 		}
 		else
 		{
-			strp[0] = '\0';
-			int fileDscr;
-			if((fileDscr = open(bufArch, O_WRONLY|O_CREAT|O_TRUNC, S_IWUSR|S_IRUSR|S_IRGRP|S_IROTH)) < 0)
+			if(strp[0] == '<')
 			{
-				printf("error opening file %s\n", bufArch);
-				exit(-1);
+				strp[0] = '\0';
+				mkdir(currp, S_IWUSR|S_IRUSR|S_IXUSR|S_IRGRP|S_IWGRP|S_IXGRP|S_IROTH|S_IWOTH|S_IXOTH);
+				chdir(currp);
+				currp = unzip(inputDescriptor, buf, strp + 1, endp);
 			}
-			bufArch = strp + 1;
-			off_t *sizefile = (off_t*)bufArch;
-			bufArch = bufArch + sizeof(off_t);
-			if(write(fileDscr, bufArch, *sizefile) != *sizefile) printf("Write error\n");
-			if(close(fileDscr) < 0) printf("error closing file");
-			bufArch = bufArch + *sizefile;
+			else
+			{
+				/*создание файла*/
+				strp[0] = '\0';
+				int fileDscr;
+				if((fileDscr = open(currp, O_WRONLY|O_CREAT|O_TRUNC, S_IWUSR|S_IRUSR|S_IRGRP|S_IROTH)) < 0)
+				{
+					printf("error opening file %s\n", currp);
+					exit(-1);
+				}
+				currp = strp + 1;
+				
+				/*определение размера файла*/
+				if(endp-currp < sizeof(off_t))
+				{
+					endp = readmore(inputDescriptor, buf, currp, endp);
+					currp = buf;
+				}
+				off_t *filesize = (off_t*)currp;
+				currp = currp + sizeof(off_t);
+				
+				/*запись данных в файл*/
+				while(endp-currp < *filesize)
+				{
+					if(write(fileDscr, currp, endp-currp) != endp-currp) printf("Write error\n");
+					*filesize -= endp-currp;
+					endp = readmore(inputDescriptor, buf, endp, endp);
+					currp = buf;
+				}
+				if(write(fileDscr, currp, *filesize) != *filesize) printf("Write error\n");
+				if(close(fileDscr) < 0) printf("error closing file");
+				currp = currp + *filesize;
+			}
 		}
-	}
+	} while(currp[0] != '\0');
 	
 	chdir("..");
-	return bufArch + 1;
+	return currp + 1;
+}
+
+char* readmore(int inputDescriptor, char *buf, char *currp, char *endp)
+{
+	long int readcount;
+	memmove(buf, currp, endp-currp); //сохранение в начале буфера необработанного остатка
+	if((readcount = read(inputDescriptor, buf + (endp-currp), PART_SIZE)) < 0) //после него записывается новая прочитанная часть
+	{
+		printf("Read error\n");
+		exit(-1);
+	}
+	buf[endp-currp + readcount] = '\0';
+	return buf + (endp-currp + readcount);
 }
 
 char* searchEntry(char *str)
